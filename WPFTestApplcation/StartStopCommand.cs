6fccfa58c5f3143a2107b853baceb195eaff7ade
Hehemon <hehemon@gmail.com>
2015-06-18 12:10:37 +0300

    Added StartStopCommand

---
+++ WPFTestApplcation/StartStopCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WPFTestApplcation
+{
+    /// <summary>
+    /// Process start/stop command for manager
+    /// </summary>
+    class StartStopCommand : ICommand
+    {
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            ProcessManager.I.ChangeState();
+            // TODO: reflect interface on viewmodel
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
