988579710b8b8c1ab0abb49be9d3e29fe2d081a5
Hehemon <hehemon@gmail.com>
2015-06-18 12:25:15 +0300

    Added implementation of DetailsCommand

--- WPFTestApplcation/DetailsCommand.cs
+++ WPFTestApplcation/DetailsCommand.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
+using NLog;
 
 namespace WPFTestApplcation
 {
@@ -14,12 +16,22 @@ namespace WPFTestApplcation
     {
         public bool CanExecute(object parameter)
         {
-            throw new NotImplementedException();
+            // TODO: check if there is selected item
+            return true;
         }
 
         public void Execute(object parameter)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var id = Convert.ToInt32(parameter);
+                var message = ProcessInfo.GetProcessDetails(id);
+                MessageBox.Show(message, "Details", MessageBoxButton.OK);
+            }
+            catch (Exception e)
+            {
+                LogManager.GetCurrentClassLogger().Error("Coudln't execute details command: {0}, {1}", e.Message, e.StackTrace);
+            }
         }
 
         public event EventHandler CanExecuteChanged;
